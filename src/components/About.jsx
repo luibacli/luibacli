40391c3a3d3344b515c8f15421b139e2ebb21a58
luibacli <luibacli@gmail.com>
2023-02-22 01:12:28 +0800

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            name="about"
+            className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+        >
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        About
+                    </p>
+                </div>
+
+                <p className="text-xl mt-20">
+                    Hi, I'm Luigie, and I am a self-taught full stack developer
+                    and a career shifter. I was a social studies teacher for
+                    four years because I love talking about history, culture,
+                    people and society. At the same time I was a freelance
+                    virtual assistant as my part-time job.
+                </p>
+                <br />
+
+                <p className="text-xl">
+                    Until I discovered programming and finally found my niche. I
+                    have developed a strong understanding of various programming
+                    languages and frameworks through self-study, courses, and
+                    personal projects. While I do not have any professional
+                    experience, I am confident in my abilities and am eager to
+                    apply my skills in a real-world setting. I am a quick
+                    learner and am excited to continue growing and developing my
+                    skills as a web developer.
+                </p>
+
+                <br />
+
+                <p className="text-xl">
+                    In addition to my technical skills, I am a highly motivated
+                    and dedicated individual who is always looking for new
+                    challenges and opportunities to grow. I am a strong
+                    problem-solver and am able to think creatively to find
+                    solutions to complex issues. I am also a great team player
+                    and am able to work well with others to achieve common
+                    goals.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import githubFinder from "../assets/portfolio/githubFinder.png";
+import houseMarketplace from "../assets/portfolio/houseMarketplace.png";
+import supportDesk from "../assets/portfolio/supportDesk.png";
+import travelAdvisor from "../assets/portfolio/travelAdvisor.png";
+import todo from "../assets/portfolio/todo.png";
+import bankist from "../assets/portfolio/bankist.png";
+const Portfolio = () => {
+    const portfolios = [
+        {
+            id: 1,
+            src: supportDesk,
+            demo: "https://support-desk-luibacli.herokuapp.com/",
+            code: "https://github.com/luibacli/support-desk-MERN",
+        },
+        {
+            id: 2,
+            src: houseMarketplace,
+            demo: "https://house-marketplace-reac-tw-firebase.vercel.app/",
+            code: "https://github.com/luibacli/house-marketplace-REACTw-Firebase",
+        },
+        {
+            id: 3,
+            src: githubFinder,
+            demo: "https://my-github-finder-react-js.vercel.app/",
+            code: "https://github.com/luibacli/my-github-finder-React.js-",
+        },
+        {
+            id: 4,
+            src: todo,
+            demo: "https://todolist-mern-luibacli.herokuapp.com/",
+            code: "https://github.com/luibacli/MERN-TODOLIST-CRUD",
+        },
+        {
+            id: 5,
+            src: bankist,
+            demo: "https://bankist-app-pink.vercel.app/",
+            code: "https://github.com/luibacli/bankist-app",
+        },
+        {
+            id: 6,
+            src: travelAdvisor,
+            demo: "https://travel-advisor-api-c2lb-6ktq7kjim-luibacli.vercel.app/",
+            code: "https://github.com/luibacli/travel-advisor-API",
+        },
+    ];
+
+    return (
+        <div
+            name="portfolio"
+            className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+        >
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+                        Portfolio
+                    </p>
+                    <p className="py-6">
+                        Check out some of my works right here:
+                    </p>
+                </div>
+
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                    {portfolios.map(({ id, src, demo, code }) => (
+                        <div
+                            key={id}
+                            className="shadow-md shadow-gray-600 rounded-lg"
+                        >
+                            <img
+                                src={src}
+                                alt=""
+                                className="rounded-md duration-200 hover:scale-105"
+                            />
+                            <div className="flex items-center justify-center">
+                                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                                    <a
+                                        href={demo}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Demo
+                                    </a>
+                                </button>
+                                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                                    <a
+                                        href={code}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Code
+                                    </a>
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
